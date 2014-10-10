ab0824a9842b15e3a9316153550dfc079bb0547c
koukakaijin <koukakaijin@gmail.com>
2014-10-10 20:59:51 +0200

    Create SystemUtils.rb

---
+++ lib/Utils/SystemUtils.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/ruby
+require 'rbconfig'
+class SystemUtils
+	def os
+		@os ||= (host_os = RbConfig::CONFIG['host_os']
+			case host_os
+			when /mswin|msys|mingw|cygwin|bccwin|wince|emc/
+				:windows
+			when /darwin|mac os/
+				:macosx
+			when /linux/
+				:linux
+			when /solaris|bsd/
+				:unix
+			else
+				raise Error::WebDriverError, "unknown os: #{host_os.inspect}"
+			end
+		)
+	end
+	def clear
+		os==:windows ? system("cls") : system("clear")
+	end
+	def checkFile(file)
+		return File.file?(file)
+	end
+	#create a file with the initial value
+	def createFile(file,time)
+		File.open(file, "w+") do |f|
+			f.print time
+		end
+	end
+	def readFile(file)
+		text = ""
+		File.open(file, "r").each_line do |line|
+			return text << line
+		end
+	end
+end
